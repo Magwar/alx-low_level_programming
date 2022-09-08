86f6fd05d05affa6980abf66742d1def86c1de6f
Magwar <you@example.com>
2022-09-09 02:53:33 +0300

    Printing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Description: Printing a statement
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	puts("Programming is like building a multilingual puzzle \n");
+	return (0);
+}
