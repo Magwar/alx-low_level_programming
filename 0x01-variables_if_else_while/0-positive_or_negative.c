8bf64efca19f2d01684af41417e30d8052d28db0
Magwar <you@example.com>
2022-09-13 20:06:53 +0300

    Printing numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,13 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 /*
-* Description:main - Assign a random number each time it is executed
+* main - Assign a random number each time it is executed
 *
-*if the number is greater than 0: is positive
+* if the number is greater than 0: is positive
 *
-*if the number is less than 0: is negative
+* if the number is less than 0: is negative
 *
-*if the number is 0: is zero
+* if the number is 0: is zero
 *
 * Return: 0 if success
 */
@@ -21,15 +21,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is Positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n < 0)
 {
-printf("%d is Negative\n", n);
+printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is Zero\n", n);
+printf("%d is zero\n", n);
 }
 return (0);
 }
