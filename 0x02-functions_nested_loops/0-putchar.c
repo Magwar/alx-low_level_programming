a95afceefb07e8d740303f9b92efef23dfa7326b
Magwar <you@example.com>
2022-09-17 00:23:12 +0300

    Prints _Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* Description: main - Prints the said characters
+*
+* Return: 0 if Success
+*/
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
